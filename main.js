f5530f63be00575e51ac97e7f2713159b8b5a62f
Ben Ebel <bge1234@gmail.com>
2015-11-20 09:34:48 -0700

    initial commit

---
+++ main.js
@@ -0,0 +1,12 @@
+var phrase = prompt("Please enter a phrase");
+getAcronym(phrase);
+
+function getAcronym() {
+  var acc = phrase[0];
+  for(i=1; i < phrase.length; i++) {
+    if(phrase[i] === ' ')
+      acc += phrase[i + 1];
+  };
+  console.log("Acronymn: " + acc.toUpperCase());
+  return acc.toUpperCase();
+};
